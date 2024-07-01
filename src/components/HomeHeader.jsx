0ef37b34f98c84420adeac47e15d2734fa494967
jogi <jogi@aisthetic.co>
2024-07-01 17:21:56 +0530

    feat: header is in progress

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -10,7 +10,7 @@ export default function HomeHeader() {
           Gmail
         </Link>
         <Link href="https://image.google.com" className="hover:underline">
-          Images
+          Imagess
         </Link>
         <TbGridDots className="bg-transparent hover:bg-gray-200 rounded-full text-4xl p-2" />
         <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 transition-shadow">
